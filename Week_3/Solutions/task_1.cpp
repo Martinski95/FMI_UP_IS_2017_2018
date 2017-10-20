a72e919cccaf9d54750c3943f499f079f640a1bb
Boris Altanov <borisaltanov@gmail.com>
2017-10-20 18:53:52 +0300

    addind solutions to week 3

---
+++ Week_3/Solutions/task_1.cpp
@@ -0,0 +1,50 @@
+////////////////
+//Task 1
+//Days in a month
+////////////////
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year, month, days;
+    cout << "Въведете месец (1-12)\n";
+    cin >> month;
+    cout << "Въведете година\n";
+    cin >> year;
+    
+    bool isLeap = (year % 4 == 0 && year % 100 != 0
+                    || year % 400 == 0) ? true : false;
+    
+    // if-else implementation
+    
+    /*
+    if (month == 1 || month == 3 || month == 5 || month == 7 
+        || month == 8 || month ==10 || month == 12) {
+        days = 31;
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        days = 30;
+    } else {
+        if (isLeap) {days = 29;}
+        else {days = 28;}
+    }
+    */
+    
+    
+    // switch implementation
+    
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            days = 31; break;
+        case 4: case 6: case 9: case 11:
+            days = 30; break;
+        case 2:
+            if (isLeap) {days = 29;}
+            else {days = 28;}
+    }
+    
+    cout << "Month " << month << " has " << days << " days.\n";
+
+    return 0;
+}
--- Week_3/Solutions/task_3.cpp
+++ Week_3/Solutions/task_3.cpp
@@ -1,24 +1,25 @@
 ////////////////
-//Task 2
-//Print numbers from 1 to N
+//Task 1
+//Sum from 1 to N
 ////////////////
+
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int endNumber;
+    int endNumber, sum = 0;
     cout << "Въведете число (> 1)\n";
     cin >> endNumber;
     
     int i = 1;
     while (i <= endNumber) {
-        cout << i << " ";
+        sum += i;
         i += 1;
     }
     
-    cout << endl;
+    cout << "Сумата от 1 до " << endNumber << " e " << sum << endl;
 
     return 0;
 }
